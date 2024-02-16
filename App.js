ec125fe01b92b5d8ff9f57dfc489aa19520e8b22
shine_52 <156677365+Shineattryb@users.noreply.github.com>
2024-02-16 17:01:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import DocAlertPopup from './components/pop_up';
+import DocMainHeader from './components2/Container';
+import Box from './components2/box';
+function App() {
+  const header="Text";
+  return (
+    <div className="App">
+   
+      <DocAlertPopup/>
+{/* <DocMainHeader header={header} /> */}
+<Box/>
+    </div>
+  );
+}
+
+export default App;
